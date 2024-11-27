9916e3be78740c01b64e690618cf338a4dfbb61f
Mrowr Purr <mrowr.purr@gmail.com>
2024-11-26 18:38:00 -0800

    Use the 'ExampleHeaderOnlyLibrary'

---
+++ example.cpp
@@ -0,0 +1,10 @@
+#include <ExampleHeaderOnlyLibrary.h>
+
+#include <iostream>
+
+
+int main() {
+    auto value = get_example_header_only_library_value();
+    std::cout << "Value: " << value << std::endl;
+    return 0;
+}
